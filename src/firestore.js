3a39e369bad310c951f0911319c2aeebfe013c6f
Mario Gil <mariogillazaro@gmail.com>
2019-04-16 15:49:45 +0700

    Initialize firebase client

---
+++ src/firestore.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/firestore";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIRESTORE_DATABASE_URL,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+};
+
+firebase.initializeApp(config);
+
+export default firebase.firestore();
