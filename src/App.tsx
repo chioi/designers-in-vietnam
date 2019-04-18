1ea1d0c7bd612a2ee24bfa3f4783acc27f05b28d
Mario Gil <mariogillazaro@gmail.com>
2019-04-18 08:57:46 +0700

    Refactor the calls to the firestore client

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,47 @@ import DesignersList, { IDesigner } from "./DesignersList";
 import firestore from "./firestore";
 import TagsList, { ITag } from "./TagsList";
 
+interface IFirestoreDocument {
+  id?: string;
+}
+
+const fetchFromFirestore = <T extends IFirestoreDocument>(
+  collection: string
+): Promise<T[]> => {
+  return firestore
+    .collection(collection)
+    .get()
+    .then(querySnapshot => {
+      const latestTags: T[] = [];
+      querySnapshot.forEach(doc => {
+        latestTags.push({ id: doc.id, ...doc.data() } as T);
+      });
+      return latestTags;
+    });
+};
+
 const App = () => {
   const [error, saveError] = useState(null);
   const [designers, saveDesigners] = useState([] as IDesigner[]);
   const [tags, saveTags] = useState([] as ITag[]);
 
   useEffect(() => {
-    firestore
-      .collection("tags")
-      .get()
-      .then(querySnapshot => {
-        const latestTags = [] as ITag[];
-        querySnapshot.forEach(doc => {
-          latestTags.push({ id: doc.id, ...doc.data() } as ITag);
-        });
-        return latestTags;
-      })
+    fetchFromFirestore<ITag>("tags")
       .then(saveTags)
       .catch(saveError);
   }, [tags]);
 
   useEffect(() => {
-    firestore
-      .collection("people")
-      .get()
-      .then(querySnapshot => {
-        const latestDesigners = [] as IDesigner[];
-        querySnapshot.forEach(doc => {
-          latestDesigners.push({ id: doc.id, ...doc.data() } as IDesigner);
-        });
-        return latestDesigners;
-      })
+    fetchFromFirestore<IDesigner>("people")
       .then(saveDesigners)
       .catch(saveError);
   }, [designers]);
 
   return (
     <main className="App">
-      <header><h1>Designers in Vietnam</h1></header>
+      <header>
+        <h1>Designers in Vietnam</h1>
+      </header>
       <section>
         <TagsList tags={tags} />
       </section>
