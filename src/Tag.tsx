4f00684c03d18b95a8dd2539265863709276b443
Mario Gil <mariogillazaro@gmail.com>
2019-04-21 22:00:53 +0700

    Put the animation on screen

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -1,4 +1,5 @@
-import React, { memo, useCallback } from "react";
+import React, { memo, useCallback, useEffect, useRef } from "react";
+import Brushstroke from "./brushstroke";
 import "./Tag.css";
 
 export interface ITag {
@@ -13,14 +14,42 @@ interface ITagProps {
 }
 
 const Tag = (props: ITagProps) => {
+  const id = `${props.tag.name}-tag`;
+  const ref = useRef(null);
+  useEffect(() => {
+    const height = 100;
+    const width = 100;
+    const optionsBackground = {
+      animation: "points",
+      color: "#F5A623",
+      duration: 0.1,
+      height: height - 16,
+      inkAmount: 3,
+      points: 4,
+      queue: true,
+      root: ref.current,
+      size: 16,
+      width: width - 8
+    };
+
+    if (ref.current) {
+      const bs = new Brushstroke(optionsBackground);
+      if (props.isSelected) {
+        bs.draw();
+      } else {
+        bs.clear();
+      }
+    }
+  }, [props.isSelected]);
+
   const onClick = useCallback(() => {
     props.onClick(props.tag);
   }, []);
 
   return (
-    <li className={`tag ${props.isSelected ? "tag--selected" : ""}`}>
+    <li ref={ref} className={`tag ${props.isSelected ? "tag--selected" : ""}`}>
       <label
-        id={`${props.tag.name}-tag`}
+        id={id}
         htmlFor={`${props.tag.name}-input`}
         className="tag-content"
         onClick={onClick}
