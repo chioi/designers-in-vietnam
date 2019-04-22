123401dea891ceaf2daacc0b84eb9638179fcb77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 17:17:33 +0000

    Deploy chioi/designers-in-vietnam to github.com/chioi/designers-in-vietnam.git:gh-pages

---
+++ precache-manifest.995215447b3968660e35f3afb157083e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75026cdfd497b88c52821eed2b4e04ec",
+    "url": "/designers-in-vietnam/static/media/title.75026cdf.svg"
+  },
+  {
+    "revision": "9eab1aadddb90d1571d9",
+    "url": "/designers-in-vietnam/static/js/runtime~main.4492d109.js"
+  },
+  {
+    "revision": "9f671fcbc7dcea505681",
+    "url": "/designers-in-vietnam/static/js/main.ab495733.chunk.js"
+  },
+  {
+    "revision": "37fd9b51874e1b94be52",
+    "url": "/designers-in-vietnam/static/js/2.f7b0c6da.chunk.js"
+  },
+  {
+    "revision": "9f671fcbc7dcea505681",
+    "url": "/designers-in-vietnam/static/css/main.794724ed.chunk.css"
+  },
+  {
+    "revision": "94c7c92f82053405c28c925e555428bf",
+    "url": "/designers-in-vietnam/index.html"
+  }
+];
