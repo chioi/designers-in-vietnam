06f810c74f8a86073e8d751e9499cbc625a19921
Mario Gil <mariogillazaro@gmail.com>
2019-04-23 15:11:04 +0700

    Add space between header icons

--- src/MainHeader/index.tsx
+++ src/MainHeader/index.tsx
@@ -6,7 +6,7 @@ const MainHeader = () => {
   return (
     <header className="margin-bottom-regular position-relative">
       <img alt="Designers of Vietnam" src={Title} className="title-image"/>
-      <a className="header-link margin-right-big" href="/about">
+      <a className="header-link margin-right-large" href="/about">
         <span>🇻🇳</span>
       </a>
       <a className="header-link" href={process.env.REACT_APP_FORM_URL}>
